d7a4c8eaa2c26cdb48958cd4b67ba537384ff122
Barreloofy <nilskofodhk@protonmail.com>
2024-12-31 23:37:38 +0100

    Initial Commit

---
+++ monoTENKI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  monoTENKI
+//
+//  Created by Barreloofy on 12/31/24 at 11:19 PM.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ monoTENKI/monoTENKIApp.swift
@@ -0,0 +1,17 @@
+//
+//  monoTENKIApp.swift
+//  monoTENKI
+//
+//  Created by Barreloofy on 12/31/24 at 11:19 PM.
+//
+
+import SwiftUI
+
+@main
+struct monoTENKIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
