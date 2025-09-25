e7c0233b90f8e08a01ca77d277bfaa397050cdad
Barreloofy <172546110+Barreloofy@users.noreply.github.com>
2025-09-25 12:57:24 +0200

    Update asyncOnChange documentation

--- monoTENKI/Components/View/Component/AsyncOnChange.swift
+++ monoTENKI/Components/View/Component/AsyncOnChange.swift
@@ -24,16 +24,15 @@ struct AsyncOnChange<T: Equatable>: ViewModifier {
 
 
 extension View {
-  /// Executes an asynchronous closure on change of the specified value.
+  /// Adds an asynchronous task to perform on change of the specified value.
   ///
   /// After the value of `id` has changed and before the action closure is executed,
   /// signals cancellation of the previous action.
   ///
   /// - Parameters:
   ///   - id: The value to observe for changes, must conform to Equatable.
-  ///   - action: An async closure that is called after the value of `id` has changed.
-  /// - Returns: A view that executes an action when the value of `id` changes.
-  func asyncOnChange<T: Equatable>(id: T, _ action: @escaping () async -> Void) -> some View {
+  ///   - action: An asynchronous closure that is called after the value of `id` has changed.
+  func asyncOnChange(id: some Equatable, _ action: @escaping () async -> Void) -> some View {
     modifier(AsyncOnChange(id: id, action: action))
   }
 }
