5855203c961ce843fde18686338dd9562cdc3a93
Barreloofy <172546110+Barreloofy@users.noreply.github.com>
2025-10-09 15:48:11 +0200

    Improve HTTPClient

--- monoTENKI/NetworkLayer/HTTPClient.swift
+++ monoTENKI/NetworkLayer/HTTPClient.swift
@@ -6,12 +6,13 @@
 //
 
 import Foundation
+import Combine
 import os
 
 /// HTTP Client with custom decoding capability.
-struct HTTPClient {
+struct HTTPClient<Decoder> where Decoder: TopLevelDecoder, Decoder.Input == Data {
   let url: URL
-  let decoder: JSONDecoder
+  let decoder: Decoder
   let session: URLSession
 
   private let logger = Logger(
@@ -22,31 +23,28 @@ struct HTTPClient {
   ///   - url: The URL to retrieve data from.
   ///   - decoder: The decoder to use for decoding the response, `JSONDecoder()` is the default.
   ///   - session: The `URLSession` to use for network tasks, `shared` is the default.
-  init(url: URL, decoder: JSONDecoder = JSONDecoder(), session: URLSession = .shared) {
+  init(url: URL, decoder: Decoder = JSONDecoder(), session: URLSession = .shared) {
     self.url = url
     self.decoder = decoder
     self.session = session
   }
 
   /// Fetches the contents of a URL
-  /// and decodes the data into the type of `T`.
+  /// and decodes the response into an instance of `T`.
   /// - Returns: An instance of `T`.
   func fetch<T: Decodable>() async throws -> T {
     do {
       let (data, response) = try await session.data(from: url)
 
-      guard (response as? HTTPURLResponse)?.statusCode == 200 else {
-        throw URLError(.badServerResponse)
-      }
+      guard
+        let statusCode = (response as? HTTPURLResponse)?.statusCode,
+        Range(200...299).contains(statusCode)
+      else { throw URLError(.badServerResponse) }
 
       return try decoder.decode(T.self, from: data)
-
-    } catch URLError.cancelled {
-      logger.error("\(URLError(.cancelled).localizedDescription)")
-      throw URLError(.cancelled)
-
     } catch {
-      logger.error("\(error)")
+      logger.error("\(error.localizedDescription)")
+
       throw error
     }
   }
